8260b9b5f8b354d1cda1b2328d675f03979b024d
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-01-27 09:04:15 -0600

    Refactor reusable app

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,6 @@
-import type { PinoLogger } from "hono-pino"
+import createApp from "@/lib/create-app"
 
-import { logger } from "@/middlewares/logger"
-import { OpenAPIHono } from "@hono/zod-openapi"
-
-import { config } from "dotenv"
-import { expand } from "dotenv-expand"
-
-import * as httpStatusCode from "@/http/status-code"
-import * as httpStatusError from "@/http/status-error"
-
-expand(config())
-
-type AppBindings = {
-  Variables: {
-    logger: PinoLogger
-  }
-}
-
-const app = new OpenAPIHono<AppBindings>()
-app.use(logger())
+const app = createApp()
 
 app.get("/", ctx => {
   return ctx.text("Hello Hono!")
@@ -26,27 +8,6 @@ app.get("/", ctx => {
 
 app.get("/error", () => {
   throw new Error("Oh no!")
-})
-
-app.notFound(ctx => {
-  return ctx.json(
-    {
-      code: httpStatusCode.NOT_FOUND,
-      error: httpStatusError.NOT_FOUND,
-    },
-    httpStatusCode.NOT_FOUND
-  )
-})
-
-app.onError((err, ctx) => {
-  ctx.var.logger.error(err.stack)
-  return ctx.json(
-    {
-      code: httpStatusCode.INTERNAL_SERVER_ERROR,
-      error: httpStatusError.INTERNAL_SERVER_ERROR,
-    },
-    httpStatusCode.INTERNAL_SERVER_ERROR
-  )
 })
 
 export default app
---
+++ src/lib/create-app.ts
@@ -0,0 +1,39 @@
+import type { AppBindings } from "./types"
+import type { ErrorHandler, NotFoundHandler } from "hono"
+
+import { logger } from "@/middlewares/logger"
+import { OpenAPIHono } from "@hono/zod-openapi"
+
+import * as httpStatusCode from "@/http/status-code"
+import * as httpStatusError from "@/http/status-error"
+
+const notFound: NotFoundHandler = ctx => {
+  return ctx.json(
+    {
+      code: httpStatusCode.NOT_FOUND,
+      error: httpStatusError.NOT_FOUND,
+    },
+    httpStatusCode.NOT_FOUND
+  )
+}
+
+const onError: ErrorHandler = (err, ctx) => {
+  ctx.var.logger.error(err.stack)
+  return ctx.json(
+    {
+      code: httpStatusCode.INTERNAL_SERVER_ERROR,
+      error: httpStatusError.INTERNAL_SERVER_ERROR,
+    },
+    httpStatusCode.INTERNAL_SERVER_ERROR
+  )
+}
+
+export default function createApp() {
+  const app = new OpenAPIHono<AppBindings>({ strict: false })
+
+  app.use(logger())
+  app.notFound(notFound)
+  app.onError(onError)
+
+  return app
+}
